78e4846ee79ab7c71b959894125ce16a56f09b8d
Ali Nabavi <arz1379n@gmail.com>
2024-06-20 10:08:51 +0330

    Remove unnecessary androidx libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.androidx.lifecycle.runtime)
-    implementation(libs.androidx.collection)
 
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
