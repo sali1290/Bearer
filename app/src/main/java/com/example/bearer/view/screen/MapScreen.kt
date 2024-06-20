09655e61064f94eb9e11083df5b54004f884f70d
Ali Nabavi <arz1379n@gmail.com>
2024-06-20 23:27:06 +0330

    Disable zoomControls

--- app/src/main/java/com/example/bearer/view/screen/MapScreen.kt
+++ app/src/main/java/com/example/bearer/view/screen/MapScreen.kt
@@ -2,23 +2,30 @@ package com.example.bearer.view.screen
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapScreen() {
     val tehran = LatLng(35.7219, 51.3347)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(tehran, 15f)
-    }
+    val cameraPositionState =
+        rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(tehran, 15f)
+        }
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState,
-        properties = MapProperties(mapType = MapType.TERRAIN)
+        properties = MapProperties(mapType = MapType.TERRAIN),
+        uiSettings = MapUiSettings(zoomControlsEnabled = false)
     )
 }
