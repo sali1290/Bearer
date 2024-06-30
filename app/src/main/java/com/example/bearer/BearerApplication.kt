c64c4373c0638b696086cdea4f84e8f21c8ca018
Ali Nabavi <arz1379n@gmail.com>
2024-06-30 23:30:19 +0330

    Implement firebase app check

--- app/src/main/java/com/example/bearer/BearerApplication.kt
+++ app/src/main/java/com/example/bearer/BearerApplication.kt
@@ -1,7 +1,20 @@
 package com.example.bearer
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BearerApplication : Application()
+class BearerApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+        // For Play Integrity
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            PlayIntegrityAppCheckProviderFactory.getInstance()
+        )
+    }
+}
