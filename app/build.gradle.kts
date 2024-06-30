cf2acc89d2134c74cd2a09011ab826037f83988e
Ali Nabavi <arz1379n@gmail.com>
2024-06-30 23:27:29 +0330

    Add appcheck dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,6 +93,8 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
     implementation(libs.firebase.functions)
+    implementation(libs.com.google.firebase.firebase.appcheck.playintegrity2)
+    implementation(libs.firebase.appcheck.safetynet)
 
     implementation(libs.coil)
 }
